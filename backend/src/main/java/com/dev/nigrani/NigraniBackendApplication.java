1f1ce5e4aa6f8ee13d3391f73cd5fac337db9603
1amol2 <amolchandra2006@gmail.com>
2026-09-06 17:17:22 +0530

    Restructured project and add base backend files

---
+++ backend/src/main/java/com/dev/nigrani/NigraniBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.nigrani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NigraniBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NigraniBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/nigrani/NigraniBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.nigrani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NigraniBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
